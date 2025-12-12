a3d9caed6c70cf42999963b17bc61cffd1cc96e2
atharvsj07 <atharva.jadhav@tdtl.world>
2025-12-12 15:23:47 +0530

    corrected db config

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const pool = new Pool({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME || 'icici',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'fabric_test',
+    user: process.env.DB_USER || 'icici',
+    password: process.env.DB_PASSWORD || '123456',
 });
 
 // Handle pool errors
